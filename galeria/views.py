8919e71e1e86c11251185791c8aa5046d231f2c2
Silas Eduardo de Souza <silas.edu.bjg@gmail.com>
2023-10-15 20:43:20 -0300

    Trabalhando com nomes dinâmicos

--- galeria/views.py
+++ galeria/views.py
@@ -4,11 +4,19 @@ from django.http import HttpResponse
 
 from templates import *
 
+
+
 def index(request):
     return HttpResponse('<h1> Space</h1><p> Projeto em Django de um site para apresentar imagens do espaço<p>')
 
 def teste(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+        1:{ "nome" : "Nebulosa de Órion",
+            "creditos": "Nasa.org / Nasa / Hubble"},
+        2:{ "nome" : "Os pilares da criação",
+            "creditos": "Nasa.org / Nasa / Jeff Hester"},
+    }
+    return render(request, 'galeria/index.html', {"cards" : dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
