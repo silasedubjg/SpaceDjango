6809e4d362a93be0d5b7e10a2b3ccfb74f7615f6
Silas Eduardo de Souza <silas.edu.bjg@gmail.com>
2023-10-15 21:02:33 -0300

    criando banco de dados

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Fotografia(models.Model):
+    nome = models.CharField(max_length=10, null=False, blank=False)
+    creditos = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    imagem = models.CharField(max_length=150, null=False, blank=False)
+
+    def __str__(self):
+        return f"Fotografia [nome=(self.nome)]"
