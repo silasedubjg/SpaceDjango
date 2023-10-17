b794ab82d82e7c7162c982e5b5b7b60115adb624
Silas Eduardo de Souza <silas.edu.bjg@gmail.com>
2023-10-16 22:04:26 -0300

    adicionando uma coluna na tabela, criando campo do tipo choices

--- galeria/models.py
+++ galeria/models.py
@@ -3,8 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Fotografia(models.Model):
+
+    OPCOES_CATEGORIA = {
+        ("NEBULOSA", "nebulosa"),
+        ("GALAXIA", "galáxia")
+    }
     nome = models.CharField(max_length=10, null=False, blank=False)
     creditos = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=150,choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
     imagem = models.CharField(max_length=150, null=False, blank=False)
 
