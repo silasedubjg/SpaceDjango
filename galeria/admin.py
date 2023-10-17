acf4f5f0a4b7499c4feddde1b5bfa28258100845
Silas Eduardo de Souza <silas.edu.bjg@gmail.com>
2023-10-17 20:57:01 -0300

    Criando fitro para categoria e criando paginação no django-admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -8,5 +8,7 @@ class ListaFotografias(admin.ModelAdmin):
     list_display = ("id","nome", "creditos")
     list_display_links = ("id","nome", "creditos")
     search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_per_page = 10
 
 admin.site.register(Fotografia, ListaFotografias)
