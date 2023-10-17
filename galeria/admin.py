84280978d8b9e2c4d02ea17bd0841196fb7c804d
Silas Eduardo de Souza <silas.edu.bjg@gmail.com>
2023-10-16 21:56:09 -0300

    Trabalhando com a Administração do Django, criando superuser e registrando o modelo

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from galeria.models import *
+
+class ListaFotografias(admin.ModelAdmin):
+    list_display = ("id","nome", "creditos")
+    list_display_links = ("id","nome", "creditos")
+    search_fields = ("nome",)
+
+admin.site.register(Fotografia, ListaFotografias)
