dc361edb299300547cfed8ef3eed405da677f12e
Silas Eduardo de Souza <silas.edu.bjg@gmail.com>
2023-10-22 16:03:30 -0300

    extilizando meu login

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -4,12 +4,15 @@ class LoginForm(forms.Form):
     nome_login=forms.CharField(
         max_length=100,
         label="usuario",
-        required=True
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Ex: Silas E.', 'class': 'form-control'},
+    )
+        
     )
     senha=forms.CharField(
         max_length=100,
         label="Senha",
         required=True,
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(attrs={'placeholder': 'Digite sua senha', 'class': 'form-control'}),
 
     )
