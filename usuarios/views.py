6c27ce4047c6fa1ef766c7bbd32080367380f716
Silas Eduardo de Souza <silas.edu.bjg@gmail.com>
2023-10-22 16:56:41 -0300

    implementando a função cadastrar

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
@@ -6,7 +6,7 @@ from templates import *
 
 from usuarios.forms import LoginForm, cadastroForm
 
-
+from django.contrib.auth.models import User
 # Create your views here.
 
 def login(request):
@@ -15,5 +15,30 @@ def login(request):
 
 def cadastro(request):
     form = cadastroForm()
+
+    if request.method == 'POST':
+        form = cadastroForm(request.POST)
+
+        if form.is_valid:
+            if form['senha'].value != form['senha2'].value:
+                redirect('cadastro')
+
+            name=form['nome_cadastro'].value()
+            email=form['email'].value()
+            senha=form['senha'].value()
+
+            print(name+email+senha)
+
+            if User.objects.filter(username=name).exists():
+                redirect('cadastro')
+
+            usuario = User(username=name,
+                           email=email,
+                           password=senha)
+            
+            usuario.save()
+            return redirect('login')
+
+
     return render(request, 'usuarios/cadastro.html', {'form': form})
    
